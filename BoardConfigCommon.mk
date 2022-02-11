360115ad340faf3b56a1007b7cbcaab0d7c63938
CannedShroud <hypersync112@gmail.com>
2022-02-12 00:33:55 +0530

    sm8350-common: bc: Use new flags for { private, public } sepolicies

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,8 +182,8 @@ VENDOR_SECURITY_PATCH := 2021-12-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_PUBLIC_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
