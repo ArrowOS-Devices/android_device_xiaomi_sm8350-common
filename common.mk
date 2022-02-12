734fb6376d1500d8adce2d4be61ec46bbd3627cb
CannedShroud <hypersync112@gmail.com>
2022-02-12 22:21:53 +0530

    sm8350-common: build android.hardware.atrace@1.0-service

--- common.mk
+++ common.mk
@@ -33,6 +33,10 @@ PRODUCT_PACKAGES += \
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     libvolumelistener \
