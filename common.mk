29dfaf8540d7e27542a08080bd90b89bd5927eec
CannedShroud <hypersync112@gmail.com>
2022-02-12 22:17:14 +0530

    sm8350-common: Define PRODUCT_EXTRA_VNDK_VERSIONS

--- common.mk
+++ common.mk
@@ -491,6 +491,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libhidlcache.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlcache.so
 
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
 # vndservicemanager has been removed from API30 devices (aosp/1235751)
 # but we still need it for display services.
 PRODUCT_PACKAGES += \
