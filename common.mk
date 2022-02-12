b7d6505c5c8b128dbad8ccf2f5e7603666f402be
CannedShroud <hypersync112@gmail.com>
2022-02-12 22:21:08 +0530

    sm8350-common: build se_nq_extn_client

--- common.mk
+++ common.mk
@@ -364,6 +364,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+ # NQ
+PRODUCT_PACKAGES += \
+    se_nq_extn_client
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
